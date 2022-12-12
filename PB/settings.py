38521f2182206fc3e043171228ed93bd9656e783
Eddie Moon <eddiemoon@eddies-mbp.home>
2022-12-12 03:34:13 -0500

    added db

--- PB/settings.py
+++ PB/settings.py
@@ -132,7 +132,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 django_on_heroku.settings(locals())
 
 # Default primary key field type
